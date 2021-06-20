4a6d4aa98443a6b94f555e3cbf87257a1dae2bea
johnb8005 <johnb8005@users.noreply.github.com>
2021-06-20 16:00:54 +0000

    deploy: 1d70996b2fd0f2fd3fdb369d316a034d7e199743

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "351ed70a8c9ce6b3a9921c36ecc98106153fe36f";
+export const SNOWPACK_PUBLIC_GIT_SHA = "1d70996b2fd0f2fd3fdb369d316a034d7e199743";
 export const SNOWPACK_PUBLIC_URL = "/svg_ts";
 export const MODE = "development";
 export const NODE_ENV = "development";
