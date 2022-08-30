89676d26f439d37013876111e38d14c5c0ea2467
johnb8005 <johnb8005@users.noreply.github.com>
2022-08-30 11:42:01 +0000

    deploy: ec5ca6de764f3ef87e16b239ad8fc21bdd98d5ec

--- _snowpack/env.js
+++
@@ -1,5 +0,0 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "536b6c283d168a3f0e4cb1256b860ac2cb5f5b14";
-export const SNOWPACK_PUBLIC_URL = "/svg_ts";
-export const MODE = "development";
-export const NODE_ENV = "development";
-export const SSR = false;
--- config.js
+++
@@ -1,7 +0,0 @@
-import * as __SNOWPACK_ENV__ from './_snowpack/env.js';
-
-export const sha = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_GIT_SHA || "arandomsha";
-export const title = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_TITLE || "SVG Fun";
-export const basename = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_URL;
-export const ghUrl = "https://github.com/johnb8005/svg_ts";
-export const github = {sha: `${ghUrl}/commit/${sha}`, url: ghUrl};
